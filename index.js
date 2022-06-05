4b7e8450f8a29a03c24e8df821eff473b6408662
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-06-05 23:13:38 +0300

    Updated API

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-  res.json({'message': 'ok'});
+  res.json({'Hello Lucy!': 'Server'});
 })
 
 app.use('/programming-languages', programmingLanguagesRouter);
